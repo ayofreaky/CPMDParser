ecaa4d8819a833efdca0fd8d93fff253bf2223d6
фрикшоу <106597137+heyfreakshow@users.noreply.github.com>
2023-03-03 15:10:04 +0500

    Add files via upload

--- CPMDParser.py
+++ CPMDParser.py
@@ -1,32 +1,29 @@
-import hashlib, requests, os, glob
+import hashlib, glob, requests
 from pathlib import Path
 
-headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-            'authority': 'civitai.com'}
+def calcSha256(fPath):
+    sha256 = hashlib.sha256()
+    with open(fPath, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            sha256.update(chunk)
+    return sha256.hexdigest()
 
-def calcSha256(fName):
-    hashSha256 = hashlib.sha256()
-    with open(fName, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hashSha256.update(chunk)
-    return hashSha256.hexdigest()
-
-rootDir = input('Root path for LoRA checkpoints: ')
+rootDir = Path(input('Root path for LoRA checkpoints: '))
 exts = ['*.pt', '*.safetensors', '*.ckpt']
-checkpoints = [f for ext in exts for f in glob.glob(os.path.join(rootDir, ext))]
+checkpoints = [f for ext in exts for f in glob.glob(str(rootDir/ext))]
 
 i = 0
 for checkpoint in checkpoints:
     i += 1
-    chkptExt = os.path.splitext(checkpoint)[1]
-    chkptFname = os.path.basename(checkpoint)
-    if os.path.exists(checkpoint.replace(chkptExt, '.txt')) or os.path.exists(checkpoint.replace(chkptExt, '.md')):
-        print(f'[{i}/{len(checkpoints)}] Skipping "{chkptFname}" (metadata file already exists)')
+    chkptExt = Path(checkpoint).suffix
+    chkptFname = Path(checkpoint).name
+    if (rootDir/chkptFname.replace(chkptExt, '.txt')).exists() or (rootDir/chkptFname.replace(chkptExt, '.md')).exists():
+        print(f'[{i}/{len(checkpoints)}] [-] Skipping "{chkptFname}" (metadata file already exists)')
     else:
-        print(f'[{i}/{len(checkpoints)}] Fetching metadata for "{chkptFname}"..')# fName = Path(checkpoint).stem # no extension
-        sha256hash = calcSha256(Path(checkpoint))
+        print(f'[{i}/{len(checkpoints)}] [~] Fetching metadata for "{chkptFname}"..')
+        sha256 = calcSha256(checkpoint)
 
-        r = requests.get(f'https://civitai.com/api/v1/model-versions/by-hash/{str(sha256hash).upper()}', headers=headers).json()
+        r = requests.get(f'https://civitai.com/api/v1/model-versions/by-hash/{str(sha256).upper()}').json()
         if len(r) > 1:
             modelId = r['modelId']
             modelName = r['model']['name']
@@ -39,19 +36,20 @@ for checkpoint in checkpoints:
             baseModel = r['baseModel']
             triggerWords = r['trainedWords']
 
-            r = requests.get(f'https://civitai.com/api/v1/models/{modelId}', headers=headers).json()
+            r = requests.get(f'https://civitai.com/api/v1/models/{modelId}').json()
 
             modelDesc = r['description']
             modelCreator = r['creator']['username']
             modelTags = r['tags']
 
-            with open(checkpoint.replace(chkptExt, '.md'), 'w') as f:
-                f.write(f'# **{modelName}** [{chkptName}]\n![cover]({modelCover})\n\nTags: {", ".join(modelTags)}\n\nTrigger words: {", ".join(triggerWords)}\n\n{modelDesc}\n-----\n{chkptDesc}\n\nUploaded by: {modelCreator}\n\nURL: https://civitai.com/models/{modelId}')
-            
-            with open(checkpoint.replace(chkptExt, '.png'), 'wb') as f:
-                f.write(requests.get(modelCover, headers=headers).content)
+            with open(rootDir/chkptFname.replace(chkptExt, '.md'), 'w') as f:
+                f.write(
+                    f'# **{modelName}** [{chkptName}]\n![cover]({modelCover})\n\nTags: {", ".join(modelTags)}\n\nTrigger words: {", ".join(triggerWords)}\n\n{modelDesc}\n-----\n{chkptDesc}\n\nUploaded by: {modelCreator}\n\nURL: https://civitai.com/models/{modelId}'
+                )
 
-            print(f'[{i}/{len(checkpoints)}] Metadata for "{chkptFname}" was saved as "{chkptFname.replace(chkptExt, ".md")}"\n')
+            with open(rootDir/chkptFname.replace(chkptExt, '.png'), 'wb') as f:
+                f.write(requests.get(modelCover).content)
 
+            print(f'[{i}/{len(checkpoints)}] [+] Metadata for "{chkptFname}" was saved as "{chkptFname.replace(chkptExt, ".md")}"')
         else:
-            print(f'[{i}/{len(checkpoints)}] Skipping "{chkptFname}" (no model found with sha256: {sha256hash})')
+            print(f'[{i}/{len(checkpoints)}] [-] Skipping "{chkptFname}" (unable to find any model matching the specified sha256 hash: {sha256})')
